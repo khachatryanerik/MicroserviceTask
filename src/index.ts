c58f13e871914fdfe1f63d38cb5600c01da736fd
Erik <erikxachatryan528517@gmail.com>
2021-08-11 03:33:49 +0400

    Version 1.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import User from "./user";
 import cron from "node-cron";
 import dotenv from "dotenv";
+import express from "express";
+
+const app = express();
 
 dotenv.config();
-User.signUp();
-cron.schedule("*/1 * * * *",() => User.sendMessage("Subject", "Hello World").catch(console.error));
+
+app.get("/", (req: any, res: any) => {
+    User.signUp();
+    cron.schedule("*/1 * * * *",() => User.sendMessage("Subject", "Hello World").catch(console.error));
+    res.send("Started sending!");
+});
+app.listen(3311);
